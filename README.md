5ef0071cbc20167935f9fbd04228352af23a79c0
Jared Long <jlong4223@gmail.com>
2021-01-26 23:56:39 -0600

    adds route

---
+++ README.md
@@ -0,0 +1,3 @@
+<!-- stopped at defining the model
+https://bezkoder.com/node-js-rest-api-express-mysql/
+ -->
--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 // requires modules
-const express = require('express')
-const logger = require('morgan')
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const logger = require("morgan");
+const app = express();
+require("dotenv").config();
+
+// requires routes
+const customerRouter = require("./routes/customers");
 
 // middleware
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(logger('dev'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
 
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to the application" });
+});
 
-app.get('/', (req, res) =>{
-    res.json({message: "welcome to the application"})
-})
+// requiring and using routes
+app.use("/", customerRouter);
 
-const port = 3000
-app.listen(port, ()=>{
-    console.log(`Server is running on port: ${port}`)
-})
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
