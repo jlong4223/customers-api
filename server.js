555166c21a57e4bec75c53671ef3071c5f1fda18
Jared Long <jlong4223@gmail.com>
2021-01-26 21:35:40 -0600

    adds morgan and dotenv

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+// requires modules
 const express = require('express')
+const logger = require('morgan')
 const app = express()
+require('dotenv').config()
 
+// middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(logger('dev'))
+
 
 app.get('/', (req, res) =>{
     res.json({message: "welcome to the application"})
